f589f1b288d264a450305ad2a461c43c4b471282
ldravidu-wsl <ldravidu@gmail.com>
2025-03-21 19:22:13 +0530

    Implement user retrieval endpoints with controller functions for getting all users and a specific user

--- routes/user.routes.js
+++ routes/user.routes.js
@@ -1,13 +1,10 @@
 import { Router } from 'express';
+import { getUser, getUsers } from '../controllers/user.controller.js';
 
 const userRouter = Router();
 
-userRouter.get('/', (req, res) => {
-    res.send({title: 'GET all users'});
-});
-userRouter.get('/:id', (req, res) => {
-    res.send({title: 'GET user details'});
-});
+userRouter.get('/', getUsers);
+userRouter.get('/:id', getUser);
 userRouter.post('/', (req, res) => {
     res.send({title: 'CREATE a new user'});
 });
