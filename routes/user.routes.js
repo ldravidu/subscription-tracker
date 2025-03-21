e415ea556f71454e2db18757772a218396a65c9a
ldravidu-wsl <ldravidu@gmail.com>
2025-03-21 19:42:40 +0530

    Add authorization middleware and protect user retrieval route

--- routes/user.routes.js
+++ routes/user.routes.js
@@ -1,10 +1,10 @@
 import { Router } from 'express';
 import { getUser, getUsers } from '../controllers/user.controller.js';
-
+import authorize from '../middlewares/auth.middleware.js';
 const userRouter = Router();
 
 userRouter.get('/', getUsers);
-userRouter.get('/:id', getUser);
+userRouter.get('/:id', authorize, getUser);
 userRouter.post('/', (req, res) => {
     res.send({title: 'CREATE a new user'});
 });
